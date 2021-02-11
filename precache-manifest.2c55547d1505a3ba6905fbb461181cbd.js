bda53e2ab7cc6c50f4b2ca140455fb8d4560ebac
Cristian Sagula <csagula@litebox.ai>
2021-02-11 19:42:00 -0300

    Updates

---
+++ precache-manifest.2c55547d1505a3ba6905fbb461181cbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edecd24460363592115126dca8cd9913",
+    "url": "./index.html"
+  },
+  {
+    "revision": "be186051db907743372c",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "0c92b450e7ba48b4f4a5",
+    "url": "./static/js/2.ed13ff85.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.ed13ff85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be186051db907743372c",
+    "url": "./static/js/main.e916969d.chunk.js"
+  },
+  {
+    "revision": "fcc5b2a4dffec523d140",
+    "url": "./static/js/runtime-main.7c048f60.js"
+  }
+]);
